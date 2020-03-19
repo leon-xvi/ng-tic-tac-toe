bc2abe47be4202dc0bd64d623396c71033124332
Justin Walker <walkerjt93@gmail.com>
2020-03-19 10:44:19 -0400

    - deleted HTML boilerplate generated by angular, leaving only the <router-outlet> - generated a square component, with inline styles and template, no test file, removed onInit imports and references from the file

---
+++ src/app/square/square.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-square',
+  template: `
+    <p>
+      square works!
+    </p>
+  `,
+  styles: []
+})
+export class SquareComponent {
+  @Input() value: 'X' | 'O';
+}
